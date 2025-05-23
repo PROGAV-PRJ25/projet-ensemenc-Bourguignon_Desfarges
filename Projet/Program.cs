ca91b3043c2ede9abc8ce7775ab4ae94e571d283
DesfargesYann <ydesfarges@ensc.fr>
2025-05-23 23:53:39 +0200

    changment de nom de variable causant un pb dans les tests

--- Projet/Program.cs
+++ Projet/Program.cs
@@ -7,49 +7,49 @@ j.Jouer();
 
 
 
-//  MeteoNationFeu m = new MeteoNationFeu();
-// Console.WriteLine("Température de la semaine " + m.Temperature);
-// Console.WriteLine("Chance sur 10 qu'ils pleuvent : " + m.Pluie);
-// m.AppliquerMeteoTour();
-// Console.WriteLine("Nouvelle température " + m.Temperature);
-// if (m.IlPleut)
-// {
-//     Console.WriteLine("Il pleut aujourd'hui");
-// }
-// else
-// {
-//     Console.WriteLine("Il ne pleut pas");
-// }
+ MeteoNationFeu m = new MeteoNationFeu();
+Console.WriteLine("Température de la semaine " + m.Temperature);
+Console.WriteLine("Chance sur 10 qu'ils pleuvent : " + m.Pluie);
+m.AppliquerMeteoTour();
+Console.WriteLine("Nouvelle température " + m.Temperature);
+if (m.IlPleut)
+{
+    Console.WriteLine("Il pleut aujourd'hui");
+}
+else
+{
+    Console.WriteLine("Il ne pleut pas");
+}
 
 
-// Potager p = new Potager(m);
+Potager po = new Potager(m);
 
-// p.AfficherPotager();
+po.AfficherPotager();
 
-// p.ChoisirPlanter();
+po.ChoisirPlanter();
 
-// p.AfficherPotager();
+po.AfficherPotager();
 
-// p.JouerModeUrgence();
+po.JouerModeUrgence();
 
-// p.AfficherPotager();
+po.AfficherPotager();
 
-// TerrainArgile ter = new TerrainArgile();
-// Console.WriteLine(ter.HumiditeSol);
+TerrainArgile ter = new TerrainArgile();
+Console.WriteLine(ter.HumiditeSol);
 
-// TerrainTerre test = new TerrainTerre();
-// PlanteCactus p = new PlanteCactus();
-// CasePotager ca = new CasePotager(test, m, p);
-// Console.WriteLine(p.Morte);
-// p.TourPlante();
-// Console.WriteLine(p.Morte);
-// Console.WriteLine(p.Age);
-// Console.WriteLine(p.EtatSante);
-// p.TourPlante();
-// Console.WriteLine("morte" +p.Morte);
-// Console.WriteLine(p.Age);
-// Console.WriteLine("sante" +p.EtatSante);
-// Console.WriteLine("dessechee" +p.Dessechee);
-// Console.WriteLine("temp" + p.BonneTemp);
-// Console.WriteLine(p.Age);
+TerrainTerre test = new TerrainTerre();
+PlanteCactus p = new PlanteCactus();
+CasePotager ca = new CasePotager(test, m, p);
+Console.WriteLine(p.Morte);
+p.TourPlante();
+Console.WriteLine(p.Morte);
+Console.WriteLine(p.Age);
+Console.WriteLine(p.EtatSante);
+p.TourPlante();
+Console.WriteLine("morte" +p.Morte);
+Console.WriteLine(p.Age);
+Console.WriteLine("sante" +p.EtatSante);
+Console.WriteLine("dessechee" +p.Dessechee);
+Console.WriteLine("temp" + p.BonneTemp);
+Console.WriteLine(p.Age);
 
